6b8bd255e6b784eaa909855f527b474e5ee1a123
Tixie <admin@glitch.family>
2019-12-03 22:18:10 +0100

    add random album feature

---
+++ assets/js/random.js
@@ -0,0 +1,25 @@
+class GoRandomAlbum {
+  constructor () {
+    this.urls = window.albumsUrls
+    this.btn = document.querySelector('[data-random="btn"]')
+  }
+
+  init () {
+    this.bindEvents()
+  }
+
+  bindEvents () {
+    if (this.btn && this.urls) {
+      this.btn.addEventListener('click', () => {
+        this.getRandomUrl()
+      })
+    }
+  }
+
+  getRandomUrl() {
+    const index = Math.floor(Math.random()*(this.urls.length+1));
+    window.location.href = this.urls[index]
+  }
+}
+
+new GoRandomAlbum().init()
